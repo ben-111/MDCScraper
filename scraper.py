43228f4aba0fd441eaa1cee5ef75cf5e6d8e6379
BenL <benl@benstuff.tk>
2022-10-15 19:14:54 +0300

    Add threading

--- scraper.py
+++ scraper.py
@@ -1,15 +1,21 @@
 import json
+import logging
 import os
-from bs4 import BeautifulSoup
+from queue import Queue
+from threading import Thread
 from urllib.error import HTTPError
 from urllib.request import urlopen
-import logging
+
+from bs4 import BeautifulSoup
 
 BASE_URL = 'https://www.microsoft.com/en-us/download/details.aspx?id={}'
 RESULTS_FILE = 'results.json'
 
+THREAD_POOL_SIZE = 20
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
 
 def find_download_header(soup: BeautifulSoup) -> str:
     div = soup.find('div', class_='download-header')
@@ -56,14 +62,39 @@ def save_results(results):
         json.dump(results, rf)
 
 
+def thread_target(ids: Queue, results: Queue):
+    while True:
+        _id = ids.get()
+        result = scrape(_id)
+        results.put((_id, result))
+
+
 def main():
     results = get_results()
-    current_id = max(int(i) for i in results.keys()) if len(results.keys()) > 0 else 0
+    current_id = 1
+    if len(results.keys()) > 0:
+        current_id = max(int(i) for i in results.keys()) + 1
+    
+    id_queue = Queue()
+    result_queue = Queue()
+    threads = [Thread(target=thread_target, args=(id_queue, result_queue), daemon=True)
+               for _ in range(THREAD_POOL_SIZE)]
+    for t in threads:
+        t.start()
+    
     try:
         while True:
-            current_id += 1
-            results[current_id] = scrape(current_id)
-            if current_id % 50 == 0:
+            # Populate id_queue
+            if id_queue.qsize() < 5000:
+                for _id in range(current_id, current_id+10000):
+                    id_queue.put(_id)
+                current_id = current_id + 10000
+
+            # Add to results
+            if result_queue.qsize() > 50:
+                for _ in range(50):
+                    _id, result = result_queue.get()
+                    results[_id] = result
                 save_results(results)
     finally:
         save_results(results)
